12d41dc18a83d8f785a497fcbf91a8508a9afe8b
Lisa Wild <lisakwild@gmail.com>
2024-02-19 15:43:58 -0800

    add a require for the setupInput function

--- play.js
+++ play.js
@@ -1,25 +1,9 @@
 const net = require("net");
 const connect = require("./client");
+const setupInput = require("./input");
 
 console.log("Connecting ...");
 connect();
 
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);   //this is the key strokes made by client/player
-  return stdin;
-};
-
-const handleUserInput = function (data) {
-  if (data === '\u0003') {
-    process.exit();
-  }
-  console.log("data: ", data);
-};
-
 setupInput();
+
