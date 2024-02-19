b93e5f95d776e42869fedf34fb46a3d708da25a9
Lisa Wild <lisakwild@gmail.com>
2024-02-19 15:43:13 -0800

    move user input code into a separate input file and export the function

---
+++ input.js
@@ -0,0 +1,22 @@
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);   //this is the key strokes made by client/player
+  return stdin;
+};
+
+const handleUserInput = function (data) {
+  if (data === '\u0003') {
+    process.exit();
+  }
+  console.log("data: ", data);
+};
+
+setupInput();
+
+module.exports = setupInput;
