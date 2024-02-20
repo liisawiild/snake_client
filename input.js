806e38f12747798954c5a58561f628ed27487b06
Lisa Wild <lisakwild@gmail.com>
2024-02-20 09:45:03 -0800

    add global connection variable and pull it into the setupInput to make it accessible to the handleUserInput

--- input.js
+++ input.js
@@ -1,12 +1,15 @@
 
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
+let connection;
+
+const setupInput = (conn) => {
+  connection = conn;                   //connection object that now can be used to interact with the server
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on("data", handleUserInput);   //this is the key strokes made by client/player
+  stdin.on("data", handleUserInput);   //data is the key strokes/conn.write made by client/player
   return stdin;
 };
 
@@ -14,9 +17,23 @@ const handleUserInput = function (data) {
   if (data === '\u0003') {
     process.exit();
   }
-  console.log("data: ", data);
-};
+//this writes to the server, but it does not mean that it prints (the server has it's own reaction in snek-multiplayer)
+  if (data === 'w') {
+    connection.write("Move: up");                  
+  }
 
-setupInput();
+  if (data === 'a') {
+    connection.write("Move: left");
+  }
+
+  if (data === 's') {
+    connection.write("Move: down");
+    }
+  
+  if (data === 'd') {
+    connection.write("Move: right");
+  }
+  
+};
 
 module.exports = setupInput;
